e49075887eb2ce032662feca989000763d89feca
Aditya <aditya.sanghi@schoolnetindia.com>
2025-11-27 14:34:55 +0530

    Remove dist

--- dist/utils/time.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isOlderThan = isOlderThan;
-function isOlderThan(date, hours) {
-    return (new Date().getTime() - new Date(date).getTime()) > hours * 60 * 60 * 1000;
-}
